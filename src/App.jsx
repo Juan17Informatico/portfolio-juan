7dc4f66a9339f1c295e730a13c294a4f86a13f2f
Juan17Informatico <juancampuzano2356@gmail.com>
2025-04-04 20:39:32 -0500

    Config Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export const App = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white">
+            <h1 className="text-3xl font-bold">Tailwind está funcionando 🚀</h1>
+        </div>
+    )
+}
